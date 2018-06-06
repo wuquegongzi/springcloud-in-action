d65dc4cbc945088a4cd6c9c0945e8f4da7177b57
Leon <swchenminglei@163.com>
2018-06-06 18:20:15 +0800

    配置中心-git版

---
+++ leon-srv-config-git/src/main/java/com/iwufang/srvconfig/SrvConfigApplication.java
@@ -0,0 +1,12 @@
+package com.iwufang.srvconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrvConfigApplication.class, args);
+    }
+}
