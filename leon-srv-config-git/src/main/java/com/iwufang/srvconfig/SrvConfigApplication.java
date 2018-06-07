a14282d0ec87668e25e14f4dcee46adff3e1308a
Leon <swchenminglei@163.com>
2018-06-07 10:16:49 +0800

    Add files via upload

--- leon-srv-config-git/src/main/java/com/iwufang/srvconfig/SrvConfigApplication.java
+++ leon-srv-config-git/src/main/java/com/iwufang/srvconfig/SrvConfigApplication.java
@@ -2,8 +2,17 @@ package com.iwufang.srvconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @author leon
+ */
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+@RefreshScope
 public class SrvConfigApplication {
 
     public static void main(String[] args) {
